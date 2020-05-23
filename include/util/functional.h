4e3a581238232390ba942bf269ecf0dc7b73ce65
Maximiliano Pin <mxcpin@gmail.com>
2020-05-23 11:12:54 +0200

    Binder improved and documented

--- include/util/functional.h
+++ include/util/functional.h
@@ -1,17 +1,21 @@
 #pragma once
 
 #include <functional>
+#include <utility>
 
 namespace util {
 
   template<typename T>
   struct Binder;
 
-  template<typename C, typename... Args>
-  struct Binder<void (C::*)(Args...)> {
-    auto operator()(void (C::*func)(Args...), Args... args) const {
+  // std::bind wrapper for member functions; the object argument is left free;
+  // the other arguments are converted to the argument types of the given
+  // function; no arguments are passed by reference (as with std::bind)
+  template<typename C, typename R, typename... Args>
+  struct Binder<R (C::*)(Args...)> {
+    auto operator()(R (C::*func)(Args...), Args... args) const {
       using namespace std::placeholders;
-      return std::bind(func, _1, args...);
+      return std::bind(func, _1, std::forward<Args>(args)...);
     }
   };
 
