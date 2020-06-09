83fce26ccc0e419d5fcaae9c7500c09a765f012b
Maximiliano Pin <mxcpin@gmail.com>
2020-06-09 21:23:54 +0200

    map mirroring

--- src/rts/Map.cpp
+++ src/rts/Map.cpp
@@ -3,10 +3,25 @@
 #include "rts/Entity.h"
 #include "util/fs.h"
 
+#include <algorithm>
 #include <cassert>
 
+namespace {
+  std::vector<std::string> expand(std::vector<std::string> lines) {
+    if (!lines.empty() && lines.back() == "@MIRROR-VH") {
+      lines.pop_back();
+      auto mirror = lines;
+      std::reverse(mirror.begin(), mirror.end());
+      for (auto& line : mirror)
+        std::reverse(line.begin(), line.end());
+      lines.insert(lines.end(), mirror.begin(), mirror.end());
+    }
+    return lines;
+  }
+}
+
 rts::Map::Map(const World& world, const CellCreator& creator, std::istream&& is)
-  : Map{world, creator, util::fs::readLines(is)} {
+  : Map{world, creator, expand(util::fs::readLines(is))} {
 }
 
 rts::Map::Map(const World& world, const CellCreator& creator, const std::vector<std::string>& lines)
