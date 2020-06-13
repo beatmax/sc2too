25379af2d63918252d1c943d1be0f7d65ad3158f
Maximiliano Pin <mxcpin@gmail.com>
2020-06-13 14:14:32 +0200

    adjusted menu keys

--- src/ui/MenuImpl.cpp
+++ src/ui/MenuImpl.cpp
@@ -1,12 +1,13 @@
 #include "MenuImpl.h"
 
 void ui::MenuImpl::print(const Menu& menu, WINDOW* win) {
-  mvwprintw(win, 0, 0, "F10:continue Q:quit");
+  mvwprintw(win, 0, 0, "G:return to game  Q:quit");
 }
 
 void ui::MenuImpl::processInput(Menu& menu, bool& quit) {
   quit = false;
   switch (getch()) {
+    case 'g':
     case KEY_F(10):
       menu.hide();
       break;
