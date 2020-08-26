8cc5474219e1ab9a5d592a67ab8789001c7fcd74
Maximiliano Pin <mxcpin@gmail.com>
2020-08-26 11:46:36 +0200

    restore environment on failed assert

--- src/ui/IO.cpp
+++ src/ui/IO.cpp
@@ -3,12 +3,31 @@
 #include "IOState.h"
 #include "X.h"
 
+#include <chrono>
 #include <clocale>
 #include <codecvt>
+#include <csignal>
+#include <cstdlib>
+#include <iostream>
 #include <locale>
+#include <thread>
+
+namespace {
+  ui::IO* gIO{};
+
+  void abrtHandler(int) {
+    using namespace std::chrono_literals;
+    std::cout << "Terminating in 10 seconds..." << std::endl;
+    std::this_thread::sleep_for(10s);
+    gIO->~IO();
+    std::_Exit(EXIT_FAILURE);
+  }
+}
 
 ui::IO::IO() : state_{std::make_unique<IOState>()}, input{*state_}, output{*state_} {
-  setlocale(LC_ALL, "en_US.UTF-8");
+  gIO = this;
+  std::signal(SIGABRT, abrtHandler);
+  std::setlocale(LC_ALL, "en_US.UTF-8");
   std::locale utf8loc{"en_US.UTF-8"};
   std::locale::global(std::locale{utf8loc, new std::codecvt_utf8<wchar_t>});
   X::init();
